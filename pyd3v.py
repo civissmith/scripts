a9aa38e61ed49b2a0da40ef738c56e0593faf4b6
Phil Smith <civis.smith@gmail.com>
2014-04-21 20:12:50 -0600

    Update pyd3v.py
    Added argparse and crude check for versions.

--- pyd3v.py
+++ pyd3v.py
@@ -14,28 +14,36 @@
 # $Id: $
 #
 ################################################################################
+import argparse
+import sys
+
 #
 # main(): Main entry point of this program
 #{
-def main():
-  mp3_name = "Enjoy.mp3"
-  mp3_file = open( mp3_name, 'rb' )
-  mp3_data = mp3_file.read()
+def main( args ):
 
-  id3_tag =  mp3_data[:3]
-  v_major, v_minor = ord(mp3_data[3]), ord(mp3_data[4])
-  flags = mp3_data[5]
+  for each in args.files:
+    mp3_name = each
+    mp3_file = open( mp3_name, 'rb' )
+    mp3_data = mp3_file.read()
+
+    id3_tag =  mp3_data[:3]
+    v_major, v_minor = ord(mp3_data[3]), ord(mp3_data[4])
+    flags = mp3_data[5]
   
-  # TIT2
-  print get_data("TIT2", mp3_data)
+    if int(v_major) < 3:
+        print "Could not process: " + each
+        return
+    # TIT2
+    print get_data("TIT2", mp3_data)
 
-  # TPE1
-  print get_data("TPE1", mp3_data)
+    # TPE1
+    print get_data("TPE1", mp3_data)
 
-  # TALB
-  print get_data("TALB", mp3_data)
+    # TALB
+    print get_data("TALB", mp3_data)
 
-  mp3_file.close()
+    mp3_file.close()
   
 #}
 #
@@ -46,6 +54,8 @@ def main():
 #{
 def get_data( tag, mp3 ):
   data = mp3.find(tag)
+  if data < 0:
+    raise TypeError
   tag_len = mp3[data+4:data+8]
   tag_len = get_int_from_synch(tag_len)
   title_str = mp3[data+10:data+10+tag_len]
@@ -97,4 +107,16 @@ def get_int_from_synch( synch ):
 # Invocation Check:
 #
 if __name__ == "__main__":
-  main()
+   #
+   # Parse the command line arguments
+   #
+   descStr="""
+   Process FILE to extract enough data to create an entry in the music library.
+   """
+   progName = sys.argv[0]
+   parser = argparse.ArgumentParser(prog=progName ,description=descStr)
+   parser.add_argument('-l','--library', help='Path to music library')
+   parser.add_argument('files', nargs='+', help='Files to be processed')
+   args = parser.parse_args()
+
+   main(args)
