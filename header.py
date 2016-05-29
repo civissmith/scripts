c3aec0df977b69c40fa77983188ddd0d6a10c21e
Phil Smith <phil.smith@gatech.edu>
2016-05-29 01:55:15 -0400

    Cleanup name conflict
     - Not really worth mentioning, but changed file to file_.

--- header.py
+++ header.py
@@ -73,6 +73,7 @@ def get_shell_locations(shells):
 #} End of get_shell_locations()
 
 def run(args):
+#{
 
     """
     Entry point for script execution. Passed in arguments, creates output file.
@@ -92,12 +93,12 @@ def run(args):
     #
     # Process each file
     #
-    for file in args.files:
+    for file_ in args.files:
 
         #
         # Determine the target language
         #
-        lang = check_language(file)
+        lang = check_language(file_)
 
         #
         # A correction is needed to determine which basic shell is requested
@@ -109,26 +110,26 @@ def run(args):
         # Remove the '3' from Python3 modules so import still works
         #
         if lang == 'python3':
-            file = file[:-1]
+            file_ = file_[:-1]
 
         commentChar = set_comment_chars(lang)
 
         #
         # Check to see if the file exists
         #
-        if os.path.isfile(file):
+        if os.path.isfile(file_):
             #
             # If the file already exists, move it out of the way temporarily
             #
-            tempFile = '.' + file + '.tmp'
+            tempFile = '.' + file_ + '.tmp'
             if os.path.isfile(tempFile):
                 print "Removing temp file: %s" % tempFile
                 os.remove(tempFile)
-            os.rename(file, tempFile)
+            os.rename(file_, tempFile)
         else:
             tempFile = ''
 
-        outFile = open(file, 'w')
+        outFile = open(file_, 'w')
         #
         # Start filling the file
         #
@@ -142,7 +143,7 @@ def run(args):
             mod = mod | stat.S_IRGRP | stat.S_IWGRP
             mod = mod | stat.S_IROTH
 
-        os.chmod(file, mod)
+        os.chmod(file_, mod)
         openCharSet = []
         closeCharSet = []
         openCharSet.append( commentChar[0] )
@@ -192,7 +193,7 @@ def run(args):
             print_line(outFile, args.columns, closeCharSet )
 
         print_line(outFile, args.columns, openCharSet )
-        outFile.write( comment + ' @Title: ' + file + '\n')
+        outFile.write( comment + ' @Title: ' + file_ + '\n')
         outFile.write( comment + '\n' )
         outFile.write( comment + ' @Author: ' + args.author + '\n')
         outFile.write( comment + '\n' )
@@ -220,18 +221,18 @@ def run(args):
 
 # } End of run()
 
-def check_language( file ):
+def check_language( file_ ):
 #{
     """
     Determines the language of the file that will be created and returns it to the caller.
     """
 
     # Makefiles are special
-    if file.lower() == 'makefile':
+    if file_.lower() == 'makefile':
         return 'makefile'
 
     # Get the file name and extension
-    name_data = file.split('.')
+    name_data = file_.split('.')
     rawExt = name_data[-1]
     name = ".".join(name_data)
 
@@ -282,7 +283,7 @@ def set_comment_chars( lang ):
 #} End of set_comment_chars
 
 
-def print_line( file, len, charSet ):
+def print_line( file_, len, charSet ):
 #{
     """
     Utility function to print one line of a specified length to the file.
@@ -290,13 +291,13 @@ def print_line( file, len, charSet ):
     for i in range(0, len):
         if i == 0:
             # First character
-            file.write( charSet[0] )
+            file_.write( charSet[0] )
         elif i == len-1:
             # Last character
-            file.write( charSet[2] + '\n' )
+            file_.write( charSet[2] + '\n' )
         else:
             # Fill character
-            file.write( charSet[1] )
+            file_.write( charSet[1] )
 
 #} End of print_line()
 
@@ -304,94 +305,94 @@ def print_line( file, len, charSet ):
 ################################################################################
 # License Printers
 ################################################################################
-def print_apache( file, comment, author ):
+def print_apache( file_, comment, author ):
     """
     Prints the Apache license into the file.
     """
-    file.write( comment + ' Copyright ' + str(datetime.now().year) + ' ' + author + '\n' )
-    file.write( comment + '\n')
-    file.write( comment + ' Licensed under the Apache License, Version 2.0 (the "License");\n' )
-    file.write( comment + ' you may not use this file except in compliance with the License.\n' )
-    file.write( comment + ' You may obtain a copy of the License at\n' )
-    file.write( comment + '\n')
-    file.write( comment + '   http://www.apache.org/licenses/LICENSE-2.0\n' )
-    file.write( comment + '\n')
-    file.write( comment + ' Unless required by applicable law or agreed to in writing, software\n' )
-    file.write( comment + ' distributed under the License is distributed on an "AS IS" BASIS,\n' )
-    file.write( comment + ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n' )
-    file.write( comment + ' See the License for the specific language governing permissions and\n' )
-    file.write( comment + ' limitations under the License.\n' )
+    file_.write( comment + ' Copyright ' + str(datetime.now().year) + ' ' + author + '\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + ' Licensed under the Apache License, Version 2.0 (the "License");\n' )
+    file_.write( comment + ' you may not use this file_ except in compliance with the License.\n' )
+    file_.write( comment + ' You may obtain a copy of the License at\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + '   http://www.apache.org/licenses/LICENSE-2.0\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + ' Unless required by applicable law or agreed to in writing, software\n' )
+    file_.write( comment + ' distributed under the License is distributed on an "AS IS" BASIS,\n' )
+    file_.write( comment + ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n' )
+    file_.write( comment + ' See the License for the specific language governing permissions and\n' )
+    file_.write( comment + ' limitations under the License.\n' )
 
-def print_bsd2( file, comment, author ):
+def print_bsd2( file_, comment, author ):
     """
     Prints the BSD 2 license into the file.
     """
-    file.write( comment + ' Copyright (c) '+ str(datetime.now().year) + ', ' + author +'\n' )
-    file.write( comment + ' All rights reserved.\n' )
-    file.write( comment + '\n')
-    file.write( comment + ' Redistribution and use in source and binary forms, with or without modification,\n' )
-    file.write( comment + ' are permitted provided that the following conditions are met:\n' )
-    file.write( comment + '\n')
-    file.write( comment + '    Redistributions of source code must retain the above copyright notice, this\n' )
-    file.write( comment + '    list of conditions and the following disclaimer.\n' )
-    file.write( comment + '\n')
-    file.write( comment + '    Redistributions in binary form must reproduce the above copyright notice, \n' )
-    file.write( comment + '    this list of conditions and the following disclaimer in the documentation \n' )
-    file.write( comment + '    and/or other materials provided with the distribution.\n' )
-    file.write( comment + '\n')
-    file.write( comment + ' THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND\n' )
-    file.write( comment + ' ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED \n' )
-    file.write( comment + ' WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE \n' )
-    file.write( comment + ' DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE \n' )
-    file.write( comment + ' FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL \n' )
-    file.write( comment + ' DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR \n' )
-    file.write( comment + ' SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER \n' )
-    file.write( comment + ' CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, \n' )
-    file.write( comment + ' OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE \n' )
-    file.write( comment + ' OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n' )
+    file_.write( comment + ' Copyright (c) '+ str(datetime.now().year) + ', ' + author +'\n' )
+    file_.write( comment + ' All rights reserved.\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + ' Redistribution and use in source and binary forms, with or without modification,\n' )
+    file_.write( comment + ' are permitted provided that the following conditions are met:\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + '    Redistributions of source code must retain the above copyright notice, this\n' )
+    file_.write( comment + '    list of conditions and the following disclaimer.\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + '    Redistributions in binary form must reproduce the above copyright notice, \n' )
+    file_.write( comment + '    this list of conditions and the following disclaimer in the documentation \n' )
+    file_.write( comment + '    and/or other materials provided with the distribution.\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + ' THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND\n' )
+    file_.write( comment + ' ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED \n' )
+    file_.write( comment + ' WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE \n' )
+    file_.write( comment + ' DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE \n' )
+    file_.write( comment + ' FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL \n' )
+    file_.write( comment + ' DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR \n' )
+    file_.write( comment + ' SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER \n' )
+    file_.write( comment + ' CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, \n' )
+    file_.write( comment + ' OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE \n' )
+    file_.write( comment + ' OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n' )
 
-def print_gpl3( file, comment, author ):
+def print_gpl3( file_, comment, author ):
     """
     Prints the GPL 3 license into the file.
     """
-    file.write( comment + ' Copyright ' + str(datetime.now().year) + ' ' + author + '\n' )
-    file.write( comment + ' This program is free software: you can redistribute it and/or modify\n' )
-    file.write( comment + ' it under the terms of the GNU General Public License as published by\n' )
-    file.write( comment + ' the Free Software Foundation, either version 3 of the License, or\n' )
-    file.write( comment + ' (at your option) any later version.\n' )
-    file.write( comment + '\n')
-    file.write( comment + ' This program is distributed in the hope that it will be useful,\n' )
-    file.write( comment + ' but WITHOUT ANY WARRANTY; without even the implied warranty of\n' )
-    file.write( comment + ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n' )
-    file.write( comment + ' GNU General Public License for more details.\n' )
-    file.write( comment + '\n')
-    file.write( comment + ' You should have received a copy of the GNU General Public License\n' )
-    file.write( comment + ' along with this program.  If not, see <http://www.gnu.org/licenses/>.\n' )
+    file_.write( comment + ' Copyright ' + str(datetime.now().year) + ' ' + author + '\n' )
+    file_.write( comment + ' This program is free software: you can redistribute it and/or modify\n' )
+    file_.write( comment + ' it under the terms of the GNU General Public License as published by\n' )
+    file_.write( comment + ' the Free Software Foundation, either version 3 of the License, or\n' )
+    file_.write( comment + ' (at your option) any later version.\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + ' This program is distributed in the hope that it will be useful,\n' )
+    file_.write( comment + ' but WITHOUT ANY WARRANTY; without even the implied warranty of\n' )
+    file_.write( comment + ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n' )
+    file_.write( comment + ' GNU General Public License for more details.\n' )
+    file_.write( comment + '\n')
+    file_.write( comment + ' You should have received a copy of the GNU General Public License\n' )
+    file_.write( comment + ' along with this program.  If not, see <http://www.gnu.org/licenses/>.\n' )
 
 
-def print_mit( file, comment, author ):
+def print_mit( file_, comment, author ):
     """
     Prints the MIT license into the file.
     """
-    file.write( comment + ' Copyright (c) ' + str(datetime.now().year) + ' '+ author +'\n' )
-    file.write( comment + '\n' )
-    file.write( comment + ' Permission is hereby granted, free of charge, to any person obtaining a copy\n' )
-    file.write( comment + ' of this software and associated documentation files (the "Software"), to deal\n' )
-    file.write( comment + ' in the Software without restriction, including without limitation the rights\n' )
-    file.write( comment + ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n' )
-    file.write( comment + ' copies of the Software, and to permit persons to whom the Software is\n' )
-    file.write( comment + ' furnished to do so, subject to the following conditions:\n' )
-    file.write( comment + '\n' )
-    file.write( comment + ' The above copyright notice and this permission notice shall be included in\n' )
-    file.write( comment + ' all copies or substantial portions of the Software.\n' )
-    file.write( comment + '\n' )
-    file.write( comment + ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n' )
-    file.write( comment + ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n' )
-    file.write( comment + ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n' )
-    file.write( comment + ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n' )
-    file.write( comment + ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n' )
-    file.write( comment + ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n' )
-    file.write( comment + ' THE SOFTWARE.\n' )
+    file_.write( comment + ' Copyright (c) ' + str(datetime.now().year) + ' '+ author +'\n' )
+    file_.write( comment + '\n' )
+    file_.write( comment + ' Permission is hereby granted, free of charge, to any person obtaining a copy\n' )
+    file_.write( comment + ' of this software and associated documentation files (the "Software"), to deal\n' )
+    file_.write( comment + ' in the Software without restriction, including without limitation the rights\n' )
+    file_.write( comment + ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n' )
+    file_.write( comment + ' copies of the Software, and to permit persons to whom the Software is\n' )
+    file_.write( comment + ' furnished to do so, subject to the following conditions:\n' )
+    file_.write( comment + '\n' )
+    file_.write( comment + ' The above copyright notice and this permission notice shall be included in\n' )
+    file_.write( comment + ' all copies or substantial portions of the Software.\n' )
+    file_.write( comment + '\n' )
+    file_.write( comment + ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n' )
+    file_.write( comment + ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n' )
+    file_.write( comment + ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n' )
+    file_.write( comment + ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n' )
+    file_.write( comment + ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n' )
+    file_.write( comment + ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n' )
+    file_.write( comment + ' THE SOFTWARE.\n' )
 
 ################################################################################
 # END License Printers
